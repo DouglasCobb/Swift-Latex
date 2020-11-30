38d1b47bf3db2281297a1e10c5730b5d3fc6cdab
Douglas Cobb <douglas.g.cobb@gmail.com>
2020-11-30 16:10:21 -0700

    Create base project

---
+++ Swift-Latex/Swift_Latex.h
@@ -0,0 +1,18 @@
+//
+//  Swift_Latex.h
+//  Swift-Latex
+//
+//  Created by Douglas Cobb on 11/30/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swift_Latex.
+FOUNDATION_EXPORT double Swift_LatexVersionNumber;
+
+//! Project version string for Swift_Latex.
+FOUNDATION_EXPORT const unsigned char Swift_LatexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Latex/PublicHeader.h>
+
+
